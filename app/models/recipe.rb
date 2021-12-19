42ca3f452426fee98aa953d92adf8f7087fa0d44
toi0621 <burningup24@gmail.com>
2021-12-19 08:13:18 +0000

    complete

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,10 @@ class Recipe < ApplicationRecord
   belongs_to :user
   attachment :image
 
+  with_options presence: true do
+    validates :title
+    validates :body
+    validates :image
+  end
+
 end
